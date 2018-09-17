ee2fe1192e02c727205173e8cd4b64818414f9a3
palagnajr <peter.alagna@revature.com>
2018-09-17 14:16:18 -0400

    Added first problem solution.

---
+++ src/main/java/com/revature/simple/JavaSimpleSolution.java
@@ -0,0 +1,82 @@
+package com.revature.simple;
+
+public class JavaSimpleSolution implements JavaSimple {
+
+	@Override
+	public int castToInt(double n) {
+		return (int) n;
+	}
+
+	@Override
+	public byte castToByte(short n) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double divide(double operandOne, double operandTwo) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEven(int n) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAllEven(int[] array) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double average(int[] array) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int max(int[] array) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int fibonacci(int n) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int[] sort(int[] array) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int factorial(int n) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int[] rotateLeft(int[] array, int n) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String isPrime(int n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean balancedBrackets(String brackets) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
